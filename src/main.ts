480727723b496c8bf93f403c1ecfd581ae0f2dc6
baophan894 <monkeyold113@gmail.com>
2025-03-07 00:18:30 +0700

    .env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  
-
-  const port = process.env.PORT || 3000; // Sử dụng giá trị từ .env
+  const port = process.env.PORT ; // Đảm bảo có giá trị mặc định nếu PORT không được cung cấp
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
 }
